4fa51806235669a57206b9fcbe8d61a576399cdb
Vadim Kurland <vadim@netcitadel.com>
2010-02-04 01:53:47 +0000

    upload to www server to separate dir

--- UsersGuide4/upload.sh
+++ UsersGuide4/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DESTINATION="root@citadel02.netcitadel.com:/var/www/html/fwbuilder/docs/users_guide"
+DESTINATION="root@citadel02.netcitadel.com:/var/www/html/fwbuilder/docs/users_guide_4"
 
 rsync -avuzr --rsh=ssh \
     --exclude upload.sh \
