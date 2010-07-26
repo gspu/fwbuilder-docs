d03ff3cf9ec91f49662ee373a3bc166bf886416d
Vadim Kurland <vadim@netcitadel.com>
2010-07-26 20:19:41 +0000

    recompiled the guide, updated push scripts

--- UsersGuide4/upload-stage.sh
+++ UsersGuide4/upload-stage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DESTINATION="glitch:/var/www/docs/users_guide_4"
+DESTINATION="webcontent@citadel02.netcitadel.com:/var/www/html/fwbuilder-stage/4.0/docs/users_guide"
 
 rsync -avuzr --rsh=ssh \
     --exclude upload.sh \
--- UsersGuide4/upload.sh
+++ UsersGuide4/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DESTINATION="root@citadel02.netcitadel.com:/var/www/html/fwbuilder/4.0/docs/users_guide"
+DESTINATION="webcontent@citadel02.netcitadel.com:/var/www/html/fwbuilder/4.0/docs/users_guide"
 
 rsync -avuzr --rsh=ssh \
     --exclude upload.sh \
