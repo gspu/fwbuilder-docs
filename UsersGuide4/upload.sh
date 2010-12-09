495b1c60fb0918537df9b520fee90fc0a127bfb8
Vadim Kurland <vadim@netctiadel.com>
2010-12-09 15:53:06 -0800

    new destination directories on the web server for the users guide

--- UsersGuide4/upload.sh
+++ UsersGuide4/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DESTINATION="webcontent@citadel02.netcitadel.com:/var/www/html/fwbuilder/4.0/docs/users_guide"
+DESTINATION="webcontent@citadel02.netcitadel.com:/var/www/html/users_guide"
 
 cd html && {
     rsync -avuzr --rsh=ssh \
