037a03e5f11ef62c49be1e57a66ff329de3696b4
Vadim Kurland <vadim@netcitadel.com>
2011-02-24 15:00:54 -0800

    upload scripts

---
+++ GettingStarted/CiscoASA/upload.sh
@@ -0,0 +1,15 @@
+
+DESTINATION="webcontent@citadel02.netcitadel.com:/var/www/gs/CiscoASA/"
+
+cd html && {
+    rsync -avuzr --rsh=ssh --delete \
+        --delete-after \
+        --exclude upload.sh \
+        --exclude upload-stage.sh \
+        --exclude .svn \
+        --exclude '*.py' \
+        --exclude '*.psd' \
+        --exclude '\#*\#' \
+        --exclude .DS_Store . $DESTINATION
+}
+
