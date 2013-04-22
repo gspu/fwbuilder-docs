a03603001964cde9c0d654e72a808bb774cf70f8
Vadim Kurland <vadim@netcitadel.com>
2013-04-22 11:43:02 -0700

    removed reference to deb and rpm repositories from users guide; fixed upload script to push pdf copy of the guide to the web site

--- UsersGuide5/upload-sf.sh
+++ UsersGuide5/upload-sf.sh
@@ -3,8 +3,10 @@
 
 DESTINATION="vkurland@web.sourceforge.net:/home/project-web/fwbuilder/htdocs/4.0/docs/users_guide5/"
 
-cd html && {
+wd=$(pwd)
 
+test -d html && {
+    cd html
 
     find . -name '*.html' | while read f; do
         echo "html -> shtml in $f"
@@ -16,12 +18,18 @@ cd html && {
 
     rsync -avuzr --rsh=ssh --delete \
         --delete-after \
-        --exclude upload.sh \
-        --exclude upload-stage.sh \
+        --exclude 'upload*.sh' \
         --exclude .svn \
         --exclude '*.py' \
         --exclude '*.psd' \
         --exclude '\#*\#' \
+        --exclude '*.html.bak' \
         --exclude .DS_Store . $DESTINATION
+
+    cd $wd
+}
+
+test -f UsersGuide5.pdf && {
+    rsync -avuzr --rsh=ssh --delete --delete-after UsersGuide5.pdf $DESTINATION
 }
 
